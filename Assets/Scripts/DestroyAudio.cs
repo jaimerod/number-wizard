c79cd657724f1b97bb4d01f68e50ab69b128865a
= <=>
2019-06-19 16:02:45 -0700

    Initial Commit

---
+++ Assets/Scripts/DestroyAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAudio : MonoBehaviour
+{
+  static DestroyAudio instance = null;
+
+  void Awake()
+  {
+    if (instance != null)
+    {
+      Destroy(gameObject);
+    } else
+    {
+      instance = this;
+      DontDestroyOnLoad(transform.gameObject);
+    }
+  }
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+  [SerializeField] TextMeshProUGUI Guess;
+  [SerializeField] int MaxGuess;
+  [SerializeField] int MinGuess;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    Guess.text = RandomGuess().ToString();
+  }
+
+  public void Higher()
+  {
+    MinGuess = int.Parse(Guess.text);
+    Guess.text = RandomGuess().ToString();
+  }
+
+  public void Lower()
+  {
+    MaxGuess = int.Parse(Guess.text);
+    Guess.text = RandomGuess().ToString();
+  }
+
+  private int RandomGuess()
+  {
+    System.Random rand = new System.Random();
+    return rand.Next(MinGuess, MaxGuess + 1);
+  }
+}
